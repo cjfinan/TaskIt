7fbaea50e7513df882cd4c810a8781977d6975f0
cjfinan <cjfinan98@gmail.com>
2022-12-21 16:43:47 +0000

    [ADD] continued content and styling to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,41 +1,55 @@
 import React from "react";
-import { Container, Nav, Row } from "react-bootstrap";
+import { Container, Nav, NavbarBrand, NavLink, Row } from "react-bootstrap";
 import styles from "../styles/NavBar.module.css";
+import logo from "../assets/GhostWhite.png";
+const NavBar = () => {
+  const loggedInIcons = (
+    <>
+      <NavLink href="" className={styles.NavLink}>
+        <i className="fa-solid fa-list"></i> Profil
+      </NavLink>
+      <NavLink href="" className={styles.NavLink}>
+        <i className="fa-solid fa-list"></i> Tasks
+      </NavLink>
+      <NavLink href="" className={styles.NavLink}>
+        <i className="fa-solid fa-chalkboard"></i> Boards
+      </NavLink>
+      <NavLink href="" className={styles.NavLink}>
+        <i className="fa-solid fa-user"></i> Profile
+      </NavLink>
+      <NavLink href="" className={styles.NavLink}>
+        Logout
+      </NavLink>
+      <NavLink href="" className={styles.NavLink}>
+        <i className="fa-solid fa-gear"></i>
+      </NavLink>
+    </>
+  );
 
-function NavBar() {
+  const loggedOutIcons = (
+    <>
+      <NavLink className={styles.NavLink}>
+        <i className="fas fa-sign-in-alt"></i>
+        <span className="d-none d-sm-inline">Sign in</span>
+      </NavLink>
+      <NavLink className={styles.NavLink}>
+        <i className="fas fa-user-plus"></i>
+        <span className="d-none d-sm-inline">Sign up</span>
+      </NavLink>
+    </>
+  );
   return (
     <Container className="m-0">
       <Row>
-        <Nav className="min-vh-100 bg-dark flex-column">
-          <Nav.Item className="mt-5">
-            <Nav.Link href="" className="mt-5">
-              <i className="fa-solid fa-list"></i> Tasks
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link href="" className="mt-5">
-              <i className="fa-solid fa-chalkboard"></i>Boards
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link href="" className="mt-5">
-              <i className="fa-solid fa-user"></i>Profile
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link href="" className="mt-5 flex">
-              Logout
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link href="" className="flex">
-              <i className="fa-solid fa-gear"></i>
-            </Nav.Link>
-          </Nav.Item>
+        <Nav className={styles.Sidebar}>
+          <NavLink>
+            <img src={logo} alt="logo" height="45" className={styles.logo} />
+          </NavLink>
+          {loggedOutIcons}
         </Nav>
       </Row>
     </Container>
   );
-}
+};
 
 export default NavBar;
