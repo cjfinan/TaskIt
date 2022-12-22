04ac7f47000be3f1c3ede0b7be616c0683db7f81
cjfinan <cjfinan98@gmail.com>
2022-12-22 10:15:38 +0000

    [ADD] axios setup

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Col, Container, Row } from "react-bootstrap";
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import { Route, Switch } from "react-router-dom";
+import './api/axiosDefaults'
+import SignUpForm from "./pages/auth/SignUpForm";
 
 function App() {
   return (
@@ -15,7 +17,7 @@ function App() {
             <Col xs={8}>
               <Route exact path="/" render={() => <h1>Home</h1>} />
               <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
-              <Route exact path="/signup" render={() => <h1>Sign up</h1>} />
+              <Route exact path="/signup" render={() => <SignUpForm/>} />
             </Col>
           </Switch>
         </Row>
