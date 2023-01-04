5c71992f20179cae9a6af4f59c03952060b5adfe
cjfinan <cjfinan98@gmail.com>
2023-01-04 09:40:26 +0000

    [ADD] sign in form page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NavBar from "./components/NavBar";
 import { Route, Switch } from "react-router-dom";
 import './api/axiosDefaults'
 import SignUpForm from "./pages/auth/SignUpForm";
+import SignInForm from "./pages/auth/SignInForm";
 
 function App() {
   return (
@@ -16,7 +17,7 @@ function App() {
           <Switch>
             <Col xs={8}>
               <Route exact path="/" render={() => <h1>Home</h1>} />
-              <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
+              <Route exact path="/signin" render={() => <SignInForm/>} />
               <Route exact path="/signup" render={() => <SignUpForm/>} />
             </Col>
           </Switch>
---
+++ src/pages/auth/SignInForm.js
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import { Button, Form, Card, Alert } from "react-bootstrap";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+
+const SignInForm = () => {
+  const [signInData, setSignInData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const { username, password } = signInData;
+
+  const [errors, setErrors] = useState({});
+
+  const history = useHistory();
+
+  const handleChange = (event) => {
+    setSignInData({
+      ...signInData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await axios.post("/dj-rest-auth/login/", signInData);
+      history.push("/");
+    } catch (err) {
+      setErrors(err.response?.data);
+    }
+  };
+
+  return (
+    <div>
+      <Card>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="username">
+            <Form.Label className="d-none">Username</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Username"
+              name="username"
+              value={username}
+              onChange={handleChange}
+            />
+          </Form.Group>
+          {errors.username?.map((message, idx) => (
+            <Alert variant="warning" key={idx}>
+              {message}
+            </Alert>
+          ))}
+
+          <Form.Group controlId="password">
+            <Form.Label className="d-none">Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={password}
+              onChange={handleChange}
+            />
+          </Form.Group>
+          {errors.password?.map((message, idx) => (
+            <Alert key={idx} variant="warning">
+              {message}
+            </Alert>
+          ))}
+
+          <Button variant="primary" type="submit">
+            Sign in
+          </Button>
+          {errors.non_field_errors?.map((message, idx) => (
+            <Alert key={idx} variant="warning" className="mt-3">
+              {message}
+            </Alert>
+          ))}
+        </Form>
+      </Card>
+    </div>
+  );
+};
+
+export default SignInForm;
