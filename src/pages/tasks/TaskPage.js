213b20a42ff146514cb0bd0b7263e4998284e729
cjfinan <cjfinan98@gmail.com>
2023-01-06 14:17:32 +0000

    [ADD] api request to Task Page, create task component

--- src/pages/tasks/TaskPage.js
+++ src/pages/tasks/TaskPage.js
@@ -1,27 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Card, Col, Row } from 'react-bootstrap'
+import { useParams } from 'react-router-dom';
+import { axiosReq } from '../../api/axiosDefaults';
+import Task from './Task';
 
 const TaskPage = () => {
+    const { id } = useParams();
+    const [task, setTask] = useState({results: []})
+
+    useEffect(()=>{
+        const handleMount = async () => {
+            try{
+                const [{ data: task}] = await Promise.all([
+                    axiosReq.get(`/tasks/${id}`),
+                ])
+                setTask({result: [task] })
+                console.log(task)
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        handleMount()
+    }, [id])
   return (
-    <Col>
-      <Card>
-        Title
-        <Card.Body>Description</Card.Body>
-        <Row>
-          <Col>Start Date</Col>
-          <Col>End Date</Col>
-        </Row>
-        <Row>
-          <Col>Status</Col>
-        </Row>
-        <Row>
-          <Col>Priority</Col>
-        </Row>
-        <Row>
-          <Col>Board</Col>
-        </Row>
-      </Card>
-    </Col>
+    <Task/>
   );
 }
 
